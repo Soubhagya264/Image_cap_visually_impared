4cca5116d40518c3a3152ea55b43b696fd1435e1
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2021-11-12 19:35:47 +0530

    stage_02_completed

---
+++ src/stage_02_data_preparation.py
@@ -0,0 +1,75 @@
+import numpy as np
+from numpy import array
+import string
+from PIL import Image
+from utils.all_utils import read_yaml,create_directory
+from utils.text_img_data_preparation import load_description,clean_description,to_vocab,prepare_train_images,load_clean_descriptions,train_captions,find_vocab_size_max_len 
+import shutil
+import argparse
+import pandas as pd
+import os
+import glob
+from tqdm import tqdm
+import logging
+logging_str="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str,filemode='a')
+
+
+      
+
+def data_preparation(config_path,params_path):
+    config=read_yaml(config_path)
+    param=read_yaml(params_path)
+    artifacts=config["artifacts"]
+    token_path=os.path.join(artifacts["DATA_DIR"],artifacts["TEXT_DATA"],artifacts["TOKEN_PATH_NAME"])
+    text = open(token_path, 'r', encoding = 'utf-8').read()
+    descriptions = load_description(text)
+    clean_description(descriptions)
+    vocab = to_vocab(descriptions)
+    img_path=os.path.join(artifacts["DATA_DIR"],artifacts["IMAGE_DATA"])
+    img=glob.glob(img_path+'*.jpg')
+    train_path=os.path.join(artifacts["DATA_DIR"],artifacts["TEXT_DATA"],artifacts["TRAIN_PATH"])
+    train_images = open(train_path, 'r', encoding = 'utf-8').read().split("\n")
+    train_img=prepare_train_images(train_path,img,train_images)
+    train_descriptions = load_clean_descriptions(descriptions, train_images)
+    train_caption=train_captions(train_descriptions)
+    vocab_size ,max_length,wordtoix=find_vocab_size_max_len(vocab,train_caption)
+    lis_of_data=[vocab_size,max_length,wordtoix]
+    
+    prepaired_data_dirs=config["prepaired_data_dir"]
+    for local_dir in (prepaired_data_dirs):
+        create_directory([local_dir])
+        
+    lis_of_data_name=['vocab_size','max_length','wordtoix']     
+    for local_dir,data_path,data in zip(prepaired_data_dirs,lis_of_data_name,lis_of_data):
+        file1 = open(local_dir+'/'+data_path+'.txt','w')  
+        logging.info(f"started writing the prepared {data_path} in the {data_path+'.txt'} file ")    
+        file1.write(str(data))
+        logging.info(f"prepared data  written succesfully in the {data_path+'.txt'} file and stored in the {local_dir} dir ") 
+        file1.close()
+        
+    
+        
+        
+  
+            
+    
+
+    
+    
+  
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="config/config.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
+    parsed_args=args.parse_args()
+    try:
+        logging.info("\n >>>>>>>>>> stage two started")
+        data_preparation(config_path=parsed_args.config,params_path=parsed_args.params) 
+        logging.info("stage two completed !! all the img_text_data prepartion done and stored \n >>>>>>>>>>>>")  
+    except Exception as e:
+        logging.exception(e)
---
+++ src/utils/text_img_data_preparation.py
@@ -0,0 +1,100 @@
+
+import numpy as np
+from numpy import array
+import string
+from PIL import Image
+import argparse
+import pandas as pd
+import os
+import glob
+from tqdm import tqdm
+import logging
+
+def load_description(text):
+    logging.info("load description function called  and started loading description")
+    mapping = dict()
+    for line in text.split("\n"):
+        token = line.split("\t")
+        if len(line) < 2:  
+            continue
+        img_id = token[0].split('.')[0] 
+        img_des = token[1]              
+        if img_id not in mapping:
+            mapping[img_id] = list()
+        mapping[img_id].append(img_des)
+    logging.info(" Ended loading description")     
+    return mapping
+
+def clean_description(desc):
+    logging.info("started cleaning the description")
+    for key, des_list in desc.items():
+        for i in range(len(des_list)):
+            caption = des_list[i]
+            caption = [ch for ch in caption if ch not in string.punctuation]
+            caption = ''.join(caption)
+            caption = caption.split(' ')
+            caption = [word.lower() for word in caption if len(word)>1 and word.isalpha()]
+            caption = ' '.join(caption)
+            des_list[i] = caption
+    logging.info("Description cleaned")
+def to_vocab(desc):
+    logging.info("Extracting Vocabulary from the sentences")
+    words = set()
+    for key in desc.keys():
+        for line in desc[key]:
+            words.update(line.split())
+    logging.info("vocabulary Extracted")        
+    return words  
+
+def prepare_train_images(images,img,train_images):
+    logging.info("started prepairing  train_img") 
+    train_img=[]
+    for im in img:
+        if (im[len(images):] in train_images):
+            train_img.append(im)
+    logging.info("prepared  train_img")        
+    return train_img        
+
+def load_clean_descriptions(des, dataset):
+    logging.info("started cleaning description for training images")
+    dataset_des = dict()
+    for key, des_list in des.items():
+        if key+'.jpg' in dataset:
+            if key not in dataset_des:
+                dataset_des[key] = list()
+            for line in des_list:
+                desc = 'startseq ' + line + ' endseq'
+                dataset_des[key].append(desc)
+    logging.info("description cleaned for training images")            
+    return dataset_des        
+
+def train_captions(train_descriptions):
+    logging.info("Extracting Captions")
+    all_train_captions=[]
+    for key, val in train_descriptions.items():
+        for caption in val:
+            all_train_captions.append(caption)
+    logging.info("Captions Extracted")        
+    return all_train_captions     
+
+def find_vocab_size_max_len(vocab,all_train_captions):
+        logging.info("started finding vocab size and maximum length")
+        vocabulary = vocab
+        threshold = 10
+        word_counts = {}
+        for cap in all_train_captions:
+                for word in cap.split(' '):
+                    word_counts[word] = word_counts.get(word, 0) + 1
+        vocab = [word for word in word_counts if word_counts[word] >= threshold]         
+        ixtoword = {}
+        wordtoix = {}
+            
+        ix = 1
+        for word in vocab:
+                wordtoix[word] = ix
+                ixtoword[ix] = word
+                ix += 1
+        max_length = max(len(des.split()) for des in all_train_captions)
+        vocab_size = len(ixtoword) + 1
+        logging.info("finding vocab_size max_len is completed")
+        return vocab_size ,max_length,wordtoix       
