b64b20dd4b3629a0a82b07272d44b46e89571af3
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2021-11-16 15:29:20 +0530

    Callbacks Prepaired satage04

---
+++ src/stage_04_prepare_callbacks.py
@@ -0,0 +1,45 @@
+from utils.all_utils import read_yaml,create_directory
+from utils.callbacks import create_and_save_tensorboard_callback,create_and_save_checkpoint_callback
+import argparse
+import pandas as pd
+import os
+import shutil
+from tqdm import tqdm
+import logging
+logging_str="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str,filemode='a')
+
+
+def prepare_callbacks(config_path,params_path):
+    config=read_yaml(config_path)
+    params=read_yaml(params_path)
+    
+    artifacts=config["artifacts"]
+    artifacts_dir=artifacts["ARTIFACTS_DIR"]
+    tensorboard_log_dir=os.path.join(artifacts_dir,artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+    checkpoint_dir=os.path.join(artifacts_dir,artifacts["CHECKPOINT_DIR"])
+    Callbacks_dir=os.path.join(artifacts_dir,artifacts["CALLBACKS_DIR"])
+    
+    create_directory(
+        [tensorboard_log_dir,
+        checkpoint_dir,
+        Callbacks_dir]
+    )
+    
+    create_and_save_tensorboard_callback(Callbacks_dir,tensorboard_log_dir)
+        
+    create_and_save_checkpoint_callback(Callbacks_dir,checkpoint_dir)
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="config/config.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
+    parsed_args=args.parse_args()
+    try:
+        logging.info("\n >>>>>>>>>> stage four started")
+        prepare_callbacks(config_path=parsed_args.config,params_path=parsed_args.params) 
+        logging.info("stage four completed !! callbacks are prepaired \n >>>>>>>>>>>>")  
+    except Exception as e:
+        logging.exception(e)
